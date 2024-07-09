602f0505b71c6a651d259b8554b16632ed83a656
paul_lestyo <lestyo24@gmail.com>
2024-07-09 15:42:26 +0700

    feat: change design jumbotron

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "*.html"],
   theme: {
-    extend: {},
+    extend: {
+			keyframes: {
+				wave: {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+			},
+			animation: {
+        wave: 'wave 2s ease-in-out infinite',
+      }
+		},
   },
   plugins: [],
 }
